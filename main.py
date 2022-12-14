0a974a8041bda15707acfd40b07126c26a731633
Jaroslaw Zabuski <zabuski.jaroslaw@gmail.com>
2022-12-14 22:29:56 +0100

    Data analysis - Average bit word length, compression rate and time elapsed calculations, grouped together in a csv file of tested cases.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import csv
 import python_scrypt.data_analysis as data_analysis
-import python_scrypt.codec as codec
+from python_scrypt import codec
+
 
 def dataAnalize(data:list, alphabet:list=[], dataName:str="", showHist:bool=False, saveHist:bool=False, histogramPath:str=""):
     countData, countData_second, countData_third = data_analysis.calculateDataCount(data, alphabet)
     data_analysis.showHistogram(countData, dataName=dataName, showHist=showHist, saveHist=saveHist, pathSaveFile=histogramPath)
     entropy, entropy_second, entropy_third = data_analysis.calculateEntropy(countData, countData_second, countData_third)
-    print("Entropia '%s' = %s" % (dataName, entropy))
-    print("Entropia '%s' blokowego drugiego rzędu  = %s" % (dataName, entropy_second))
-    print("Entropia '%s' blokowego trzeciego rzędu = %s" % (dataName, entropy_third))
+
     return (entropy, entropy_second, entropy_third)
 
 def analizeDataFromPGM(filename:str, pathFile:str="", showHist:bool=False, saveHist:bool=False, histogramPath:str=""):
@@ -21,31 +21,50 @@ def analizeTextData(filename:str, pathFile:str="", showHist:bool=False, saveHist
     entropies = dataAnalize(data, list(range(256)), dataName=filename[:-4], showHist=showHist, saveHist=saveHist, histogramPath=histogramPath)
     return (data, list(range(256)), entropies)
 
-if __name__ == "__main__":
-    #nameDataPGMFiles = ["barbara.pgm", "boat.pgm", "chronometer.pgm", "geometr_05.pgm", "geometr_09.pgm", "geometr_099.pgm", "laplace_10.pgm", "laplace_20.pgm", "laplace_30.pgm", "lena.pgm", "mandril.pgm", "normal_10.pgm", "normal_30.pgm", "normal_50.pgm", "peppers.pgm", "uniform.pgm"]
-    nameDataPGMFiles = []
-    for i in nameDataPGMFiles:
-        data = analizeDataFromPGM(i, pathFile="data/", showHist=False, saveHist=True, histogramPath="histograms/")
-    
-    #nameTextData = ["text_test.txt"]
-    nameTextData = []
-    for i in nameTextData:
-        data = analizeTextData(i, pathFile="data/", showHist=True, saveHist=False, histogramPath="histograms/")
+def main():
+    results_file = open('LZW_results.csv', 'a')
+    writer = csv.writer(results_file)
 
-    print("-------------------------")
-    print("Test code:")
-    data = bytes("WYS*WYGWYS*WYSWYSG", 'UTF-8')
-    print("Before code:")
-    print(data)
-    ret = codec.code(data)
-    print("After code:")
-    print(ret)
-    print("-------------------------")
-    print("Test decode:")
-    data = ret
-    print("Before decode:")
-    print(data)
-    ret = codec.decode(data)
-    print("After decode:")
-    print(ret)
-    print("-------------------------")
+    data = ["File_name", "Max_bit_word_size", "Compression_time", "Decompression_time",
+            "Entropy", "Entropy_2", "Entropy_3",
+            "Avg_bit_word_size", "Compression_ratio"]
+    writer.writerow(data)
+
+    nameDataPGMFiles = ["barbara.pgm", "boat.pgm", "chronometer.pgm", "geometr_05.pgm", "geometr_09.pgm",
+                        "geometr_099.pgm", "laplace_10.pgm", "laplace_20.pgm", "laplace_30.pgm", "lena.pgm",
+                        "mandril.pgm", "normal_10.pgm", "normal_30.pgm", "normal_50.pgm", "peppers.pgm", "uniform.pgm"]
+    # nameDataPGMFiles = []
+    for i in nameDataPGMFiles:
+        print(i + ": ...")
+
+        data = analizeDataFromPGM(i, pathFile="data/", showHist=False, saveHist=False, histogramPath="histograms/")
+        data_final = data_analysis.processFile(data, i)
+        writer.writerow(data_final)
+
+        print(i + ": DONE")
+
+    nameTextData = ["text_test.txt"]
+    # nameTextData = []
+    for i in nameTextData:
+        print(i + ": ...")
+
+        data = analizeTextData(i, pathFile="data/", showHist=False, saveHist=False, histogramPath="histograms/")
+        data_final = data_analysis.processFile(data, i)
+        writer.writerow(data_final)
+
+        print(i + ": DONE")
+
+    print("Tests concluded. CLOSING...")
+    results_file.close()
+
+def main_testloop():
+    test_max_bit_word_size = [9,13,17,20,24,28,32]
+    for i in test_max_bit_word_size:
+        print("Current test max bit word size: " + str(i))
+        codec.c_max_dict_bit_size = i
+        main()
+
+if __name__ == "__main__":
+    main_testloop()
+    # main()
+
--- python_scrypt/data_analysis.py
+++ python_scrypt/data_analysis.py
@@ -1,8 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from time import time
 
-def readFilePGM(filename:str, pathFile:str =""):
+from python_scrypt import codec
+
+
+def printTime(start):
+    end = time()
+    duration = end - start
+    if duration < 60:
+        return str(round(duration, 2)) + "s."
+    else:
+        mins = int(duration / 60)
+        secs = round(duration % 60, 2)
+        if mins < 60:
+            return str(mins) + "m " + str(secs) + "s."
+        else:
+            hours = int(duration / 3600)
+            mins = mins % 60
+            return str(hours) + "h " + str(mins) + "m " + str(secs) + "s."
+
+
+def readFilePGM(filename: str, pathFile: str = ""):
     with open(pathFile + filename, 'rb') as pgmf:
         lines = pgmf.readlines()
 
@@ -29,7 +49,8 @@ def readFilePGM(filename:str, pathFile:str =""):
 
     return header, np.concatenate(im, axis=None)
 
-def calculateDataCount(data:list, alphabet:list=[]):
+
+def calculateDataCount(data: list, alphabet: list = []):
     countData = {}
     countDataForSecondRank = {}
     countDataForThirdRank = {}
@@ -55,7 +76,7 @@ def calculateDataCount(data:list, alphabet:list=[]):
     # Calculate counts of data blocks of second rank for entropy-2
 
     for i in range(0, len(data) - 1, 2):
-        block = np.uint32(data[i]) + (np.uint32(data[i+1] << 8))
+        block = np.uint32(data[i]) + (np.uint32(data[i + 1] << 8))
 
         if block in countDataForSecondRank.keys():
             countDataForSecondRank[block] += 1
@@ -76,7 +97,9 @@ def calculateDataCount(data:list, alphabet:list=[]):
 
     return countData, countDataForSecondRank, countDataForThirdRank
 
-def showHistogram(countData:dict, dataName:str="", showHist:bool=False, saveHist:bool=False, pathSaveFile:str=""):
+
+def showHistogram(countData: dict, dataName: str = "", showHist: bool = False, saveHist: bool = False,
+                  pathSaveFile: str = ""):
     plt.bar(list(countData.keys()), list(countData.values()), width=1)
     plt.title(dataName)
     if saveHist:
@@ -85,7 +108,8 @@ def showHistogram(countData:dict, dataName:str="", showHist:bool=False, saveHist
         plt.show()
     plt.close()
 
-def calculateEntropy(countData:dict,countData_second:dict,countData_third:dict, logBase=2):
+
+def calculateEntropy(countData: dict, countData_second: dict, countData_third: dict, logBase=2):
     countAllData = 0
     for i in countData:
         countAllData += countData[i]
@@ -99,13 +123,50 @@ def calculateEntropy(countData:dict,countData_second:dict,countData_third:dict, 
     entropy_second = 0
     for i in countData_second:
         if countData_second[i] > 0:
-            p = countData_second[i] / (countAllData/2)
+            p = countData_second[i] / (countAllData / 2)
             entropy_second += p * math.log(p, logBase)
 
     entropy_third = 0
     for i in countData_third:
         if countData_third[i] > 0:
-            p = countData_third[i] / (countAllData/3)
+            p = countData_third[i] / (countAllData / 3)
             entropy_third += p * math.log(p, logBase)
 
-    return entropy*(-1), entropy_second*(-1), entropy_third*(-1)
+    return entropy * (-1), entropy_second * (-1), entropy_third * (-1)
+
+
+def processFile(data, file_name):
+    start = time()
+    code_ret = codec.code(data[0])
+    end_code = printTime(start)
+    print("Time elapsed for CODE: " + end_code)
+
+    start = time()
+    decode_ret = codec.decode(code_ret)
+    end_decode = printTime(start)
+    print("Time elapsed for DECODE: " + end_decode)
+
+    # Line below prints the size of decompressed data in bytes -
+    # - equals the real size of file, minus ~5 bytes, for every test file.
+    # print("Size of decompressed data: " + str(decode_ret.__sizeof__()))
+
+    int_decode = [x for x in decode_ret]
+    print("Size of compressed data: " + str(code_ret.__sizeof__()))
+    print("Size of decompressed data: " + str(int_decode.__sizeof__()))
+    comp_rate = code_ret.__sizeof__() / int_decode.__sizeof__();
+    print("Compression rate: " + "{:.2%}".format(comp_rate) + " of original file.")
+
+    avg_bit_word_length = sum([x.bit_length() for x in code_ret]) / len(code_ret)
+    print("Average bit word length: " + str(avg_bit_word_length))
+
+    # True for all test files - used to check if data is properly decoded.
+    # print("Is decoded data the same as original? " + str((int_decode == data[0]).all()))
+
+    print("Entropy '%s' = %s" % (file_name, data[-1][0]))
+    print("Entropy^2 '%s' = %s" % (file_name, data[-1][1]))
+    print("Entropy^3 '%s' = %s" % (file_name, data[-1][2]))
+
+    data = [file_name, codec.c_max_dict_bit_size, end_code, end_decode, data[-1][0], data[-1][1], data[-1][2],
+            avg_bit_word_length, "{:.2%}".format(comp_rate)]
+
+    return data
